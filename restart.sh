ece28b9e2061aba0ab0576b58f40bb0aaa2ba6c7
rick <johnson19921005@163.com>
2020-09-02 13:38:51 +0800

    change sh

--- restart.sh
+++ restart.sh
@@ -1,4 +1,3 @@
-sudo su -
 cd /mysite/demo
 ps -ef | grep uwsgi | grep -v grep | awk '{print $2}'|xargs kill -9
 nohup uwsgi --http :8000 --module demo.wsgi &
--- stop.sh
+++ stop.sh
@@ -1,3 +1,2 @@
-sudo su -
 cd /mysite/demo
 ps -ef | grep uwsgi | grep -v grep | awk '{print $2}'|xargs kill -9
