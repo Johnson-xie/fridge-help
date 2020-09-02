34d885d2d3547cada4630c7bd564fc4234cb0364
rick <johnson19921005@163.com>
2020-09-02 17:29:06 +0800

    调整监控图

--- data/urls.py
+++ data/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path(r'api/temperatures/', views.get_temperature, name='temperature-data'),
     path(r'api/power/', views.get_power, name='power-data'),
     path(r'example/', views.index, name='example'),
-    path(r'power/', views.draw_power, name='draw-power')
-
+    path(r'power/', views.draw_power, name='draw-power'),
+    path(r'temperature/', views.draw_temperature, name='draw_temperature'),
 ]
--- data/views.py
+++ data/views.py
@@ -1,3 +1,6 @@
+import random
+from itertools import groupby
+
 from django.db import connection
 from django.http import HttpResponse
 from jinja2 import Environment, FileSystemLoader
@@ -77,9 +80,9 @@ def get_power(request):
 def index(request):
     c = (
         charts.Line()
-        .add_xaxis(["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"])
-        .add_yaxis("商家A", [5, 20, 36, 10, 75, 90])
-        .add_yaxis("商家B", [15, 25, 16, 55, 48, 8])
+        .add_xaxis([i for i in range(1000)])
+        .add_yaxis("商家A", [random.randint(50, 100) for i in range(1000)])
+        .add_yaxis("商家B", [random.randint(1, 50) for i in range(1000)])
         .set_global_opts(title_opts=opts.TitleOpts(title="Bar-基本示例", subtitle="我是副标题"))
     )
     return HttpResponse(c.render_embed())
@@ -108,6 +111,47 @@ def draw_power(request):
         .add_yaxis("电流", current_list)
         .add_yaxis("功率", rate_list)
         .add_yaxis("累计耗电量", consumption_list)
-        .set_global_opts(title_opts=opts.TitleOpts(title="耗电基本信息", subtitle="耗电基本信息"))
+        .set_global_opts(title_opts=opts.TitleOpts(title="测试监控", subtitle="耗电基本信息"))
+    )
+    return HttpResponse(c.render_embed())
+
+
+def draw_temperature(request):
+    start = request.GET.get('start', '2020-01-01 00:00:00')
+    end = request.GET.get('end', '2099-12-31 00:00:00')
+
+    sql = 'SELECT time, code, value FROM  tbl_temperature where time>=%s AND time<=%s order by code, time'
+    with connection.cursor() as c:
+        c.execute(sql, (start, end))
+        rows = c.fetchall()
+
+    time_list = []
+    lines = []
+
+    for key, content in groupby(rows, key=lambda x: x[1]):
+        if not time_list:
+            values = []
+            for time, _, value in content:
+                time_list.append(time)
+                values.append(value)
+        else:
+            values = [value for _, _, value in content]
+        lines.append((layer_map[key], values))
+
+    # for code, t, values in rows:
+    #     if not time_list:
+    #         time_list = t.split(',')
+    #     lines.append((layer_map[code], values.split(',')))
+
+    c = (
+        charts.Line()
+        .add_xaxis(time_list)
+        .add_yaxis(lines[0][0], lines[0][1])
+        .add_yaxis(lines[1][0], lines[1][1])
+        .add_yaxis(lines[2][0], lines[2][1])
+        .add_yaxis(lines[3][0], lines[3][1])
+        .add_yaxis(lines[4][0], lines[4][1])
+        .add_yaxis(lines[5][0], lines[5][1])
+        .set_global_opts(title_opts=opts.TitleOpts(title="测试监控", subtitle="传感器温度信息"))
     )
     return HttpResponse(c.render_embed())
